b7aaaa40b9a4afefce57830abbf4598b90ede53c
Muath Khraisat <muath.khraisat@gmail.com>
2025-02-26 11:29:13 +0300

    Update test file

--- .github/workflows/tests/sum.test.js
+++ .github/workflows/tests/sum.test.js
@@ -1,11 +1,12 @@
+// sum.js - the main file where you define the function
 function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
 
-// Jest test
-const sum = require('../sum');
+// sum.test.js - the test file
+const sum = require('../sum');  // Make sure this is required only once, and not re-declared
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-
